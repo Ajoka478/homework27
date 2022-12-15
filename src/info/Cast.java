97a86d78ee55f46c445a25bda1e7c8755bd2eba8
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-15 18:51:50 +0600

    updated tostring

--- src/info/Cast.java
+++ src/info/Cast.java
@@ -3,25 +3,27 @@ package info;
 public class Cast {
 private String fullName;
 private String role;
-public Cast(String fullName, String role){
-    this.fullName = fullName;
-    this.role = role;
-}
+//public Cast(String fullName, String role){
+//    this.fullName = fullName;
+//    this.role = role;
+//}
 
     public String getFullName() {
         return fullName;
     }
-
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
     public String getRole() {
         return role;
     }
-
+    public void setRole(String role) {
+        this.role = role;
+    }
     @Override
     public String toString() {
-        return "Info.Movie{" +
-                "full name=" + fullName +
-                ", role='" + role + '\'' +
-                '}';
+        return String.format("%-10s | %-10s", fullName, role);
+
     }
 }
