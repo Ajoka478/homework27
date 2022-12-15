9a3c52bc8ea162a597a22e5f99cde276a0f6c6ec
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-15 11:57:56 +0600

    Updated FileService class

--- src/util/FileService.java
+++ src/util/FileService.java
@@ -1,35 +1,53 @@
 package util;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import info.Movie;
-
+import com.google.gson.reflect.TypeToken;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FileService {
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    private static final Path PATH = Paths.get("data/tracks.json");
+public class FileService <T>{
+    private final Type type;
+    private final Gson gson;
 
-    public static Movie[] readFile(){
-        String read = "";
-        try{
-            read = Files.readString(PATH);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return GSON.fromJson(read, Movie[].class);
+    public FileService(Class<T> Class) {
+        this.gson = new Gson();
+        this.type = new TypeToken<ArrayList<T>>(){}.getType();
     }
 
-    public static void writeFile(Movie[] movies){
-        String str = GSON.toJson(movies);
-        try{
-            Files.write(PATH, str.getBytes());
+    public List<T> readJson(String fileName){
+        try (Reader reader = new BufferedReader(new FileReader(fileName))){
+            return gson.fromJson(reader, type);
         } catch (IOException e){
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
     }
+
+//    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+//    private static final Path PATH = Paths.get("data/movies.json");
+//
+//    public MovieCollection readFile(){
+//        Gson gson = new Gson();
+//        try(FileReader reader = new FileReader("data/movies.json")){
+//           MovieCollection movieCollection = gson.fromJson(reader, MovieCollection.class);
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
+
+//    public MovieCollection writeFile(Movie movies){
+//        String str = GSON.toJson(movies);
+//        try{
+//            Files.write(PATH, str.getBytes());
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+//        return null;
+//    }
 
 }
