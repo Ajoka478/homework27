fc22712f7198d838b767bfd23e6e6ecc8c376b9e
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-15 18:52:44 +0600

    added comparator

--- src/info/Movie.java
+++ src/info/Movie.java
@@ -1,53 +1,62 @@
 package info;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Movie {
+public class Movie implements Comparable<Movie>{
     private String name;
-    private int year;
+    private Integer year;
     private String description;
     private Director director;
-    private List<Cast> cast;
-
-    public Movie(String name, int year, String description, Director director, ArrayList<Cast> cast) {
-        this.name = name;
-        this.year = year;
-        this.description = description;
-        this.director = director;
-        this.cast = cast;
-
-    }
-
-
+    private List<Cast> cast = null;
     public String getName() {
         return name;
+    }
+    public void setName(String name) {
+        this.name = name;
     }
 
     public int getYear() {
         return year;
     }
-
+    public void setYear(Integer year) {
+        this.year = year;
+    }
     public String getDescription() {
         return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public Director getDirector() {
         return director;
     }
+    public void setDirector(Director director) {
+        this.director = director;
+    }
 
     public List<Cast> getCast() {
         return cast;
     }
+    public void setCast(List<Cast> cast) {
+        this.cast = cast;
+    }
+
 
     @Override
     public String toString() {
-        return "Info.Movie{" +
-                "name=" + name +
-                ", year='" + year + '\'' +
-                ", description='" + description + '\'' +
-                ", director='" + director + '\'' +
-                ", cast='" + cast + '\'' +
-                '}';
+        return  String.format("\n %-45s | %-5s | %-12s | %-17s | %-20s", name, year, description, director, cast);
+    }
+
+    @Override
+    public int compareTo(Movie o) {
+        return this.getYear() - o.getYear();
+
     }
 }
