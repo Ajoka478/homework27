113d50aaa609e849810c728bb1ad30604e560fb8
aizhamal_sydykova <sydykova.aizhamal@gmail.com>
2022-12-15 11:59:41 +0600

    Updated Director class

--- src/info/Director.java
+++ src/info/Director.java
@@ -3,11 +3,18 @@ package info;
 public class Director {
     private String fullName;
 
+    public Director(String fullName){
+        this.fullName = fullName;
+    }
+
     public String getFullName(){
         return fullName;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    @Override
+    public String toString() {
+        return "Info.Movie{" +
+                "full name=" + fullName +
+                '}';
     }
 }
